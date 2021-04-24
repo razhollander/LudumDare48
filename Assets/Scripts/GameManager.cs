16a0198acb8b3dda8eb7e535df7bc925661a4d68
razhollander <50023823+razhollander@users.noreply.github.com>
2021-04-25 01:14:07 +0300

    game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    //public queen
+    //public ants manager
+    //pubilc enemies manager
+    //public food manager
+    //public scene manager
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
