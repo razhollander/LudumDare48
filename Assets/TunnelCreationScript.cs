9ecd0d547b0ebdf9860e71a1038c067bffece99e
MarMeron <meron.hadarr@gmail.com>
2021-04-25 15:54:27 +0300

    push

--- Assets/TunnelCreationScript.cs
+++ Assets/TunnelCreationScript.cs
@@ -4,24 +4,50 @@ using UnityEngine;
 
 public class TunnelCreationScript : MonoBehaviour
 {
-    public GameObject tunnel;
-    LineRenderer _lineRenderer;
+    public GameObject tunnelPrefab;
+    public GameObject currentLine;
+    LineRenderer _lr;
     Ant ant;
+    bool _flag;
+    Transform[] points;
+    List<Vector2> positions;
     void Start()
     {
-        _lineRenderer = tunnel.GetComponent<LineRenderer>();
+        _lr = tunnelPrefab.GetComponent<LineRenderer>();
         ant = GetComponent<Ant>();
-        Instantiate(tunnel,transform.position,Quaternion.identity);
+        Instantiate(tunnelPrefab, transform.position,Quaternion.identity);
+   
     }
 
     void Update()
     {
-        if (ant._isMoving)
+        if (ant._isMoving && _flag)
         {
-            int index = 0;
-            index++;
-          //  _lineRenderer.positionCount;
-            _lineRenderer.SetPosition(index, transform.position);
+            CreateLine();
+            _flag = false;
         }
+
+        if (ant._isMoving && _flag == false)
+        {
+            UpdateLine(transform.position);
+        }
+    }
+
+    public void CreateLine()
+    {
+        currentLine = Instantiate(tunnelPrefab, Vector2.zero, Quaternion.identity) ;
+        _lr = currentLine.GetComponent<LineRenderer>();
+        positions.Clear();
+        positions.Add(transform.position);
+        positions.Add(transform.position);
+        _lr.SetPosition(0,positions[0]);
+        _lr.SetPosition(1,positions[1]);
+    }
+
+    public void UpdateLine(Vector2 newPos)
+    {
+        positions.Add(newPos);
+        _lr.positionCount++;
+        _lr.SetPosition(_lr.positionCount - 1 ,newPos);
     }
 }
