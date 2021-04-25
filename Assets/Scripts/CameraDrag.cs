b886ce7a14caf5d421f2a5f43f8ec0da50721465
MarMeron <meron.hadarr@gmail.com>
2021-04-25 03:29:49 +0300

    scene

--- Assets/Scripts/CameraDrag.cs
+++ Assets/Scripts/CameraDrag.cs
@@ -9,29 +9,72 @@ public class CameraDrag : MonoBehaviour
     private Vector3 dragOrigin;
 
 
+    //void Update()
+    //{
+    //    //float zoom = Input.GetAxisRaw("Mouse ScrollWheel");
+    //    //Camera.main.GetComponentInChildren<CinemachineVirtualCamera>().m_Lens.OrthographicSize += zoom * -5;
+    //    //Camera.main.GetComponentInChildren<CinemachineVirtualCamera>().m_Lens.OrthographicSize = 
+    //    //        Mathf.Clamp(Camera.main.GetComponentInChildren<CinemachineVirtualCamera>().m_Lens.OrthographicSize,3,7);
+
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        dragOrigin = Input.mousePosition;
+    //        Camera.main.transform.position = new Vector3(Input.mousePosition.x, Input.mousePosition.y,-10) ;
+    //        return;
+    //    }
+
+    //    if (!Input.GetMouseButton(0)) return;
+
+    //    //Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+    //    //Vector3 move = new Vector3(pos.x * dragSpeed, pos.y * dragSpeed, 0);
+
+    //    //transform.Translate(-move, Space.World);
+
+
+    //}
+
+    public float offset;
+    public float speed;
+    //x - min y - max
+    public Vector2 minMaxXPosition;
+    public Vector2 minMaxYPosition;
+    private float screenWidth;
+    private float screenHeight;
+    private Vector3 cameraMove;
+    // Use this for initialization
+    void Start()
+    {
+        screenWidth = Screen.width;
+        screenHeight = Screen.height;
+        cameraMove.x = transform.position.x;
+        cameraMove.y = transform.position.y;
+        cameraMove.z = transform.position.z;
+    }
+    // Update is called once per frame
     void Update()
     {
-        float zoom = Input.GetAxisRaw("Mouse ScrollWheel");
-        Camera.main.GetComponentInChildren<CinemachineVirtualCamera>().m_Lens.OrthographicSize += zoom * -5;
-        Camera.main.GetComponentInChildren<CinemachineVirtualCamera>().m_Lens.OrthographicSize = 
-                Mathf.Clamp(Camera.main.GetComponentInChildren<CinemachineVirtualCamera>().m_Lens.OrthographicSize,3,7);
-
-        if (Input.GetMouseButtonDown(0))
+        //Move camera
+        if ((Input.mousePosition.x > screenWidth - offset) && transform.position.x < minMaxXPosition.y)
         {
-            dragOrigin = Input.mousePosition;       
-            return;
+            cameraMove.x += MoveSpeed();
         }
-
-        if (!Input.GetMouseButton(0)) return;
-
-        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
-        Vector3 move = new Vector3(pos.x * dragSpeed, pos.y * dragSpeed, 0);
-
-        transform.Translate(-move, Space.World);
-
-       
+        if ((Input.mousePosition.x < offset) && transform.position.x > minMaxXPosition.x)
+        {
+            cameraMove.x -= MoveSpeed();
+        }
+        if ((Input.mousePosition.y > screenHeight - offset) && transform.position.y < minMaxYPosition.y)
+        {
+            cameraMove.y += MoveSpeed();
+        }
+        if ((Input.mousePosition.y < offset) && transform.position.y > minMaxYPosition.x)
+        {
+            cameraMove.y -= MoveSpeed();
+        }
+        transform.position = cameraMove;
     }
-
-
+    float MoveSpeed()
+    {
+        return speed * Time.deltaTime;
+    }
 
 }
