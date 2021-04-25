2a9d3177a3ee3feef91992bddf6fbe7a97a6bc35
razhollander <50023823+razhollander@users.noreply.github.com>
2021-04-25 03:04:40 +0300

    add ants

--- Assets/Scripts/AntsManager.cs
+++ Assets/Scripts/AntsManager.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class AntsManager : MonoBehaviour
 {
+    [SerializeField]
+    Ant antPrefab;
 
+    [ContextMenu("Add ant")]
+    public void AddAnt()
+    {
+        var ant = antPrefab.Get<Ant>(false);
+        ant.transform.position = GameManager.instance.queen.transform.position;
+        ant.gameObject.SetActive(true);
+    }
 }
