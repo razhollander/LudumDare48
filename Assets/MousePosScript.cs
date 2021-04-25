39678d643afeceec18640931fdca7981e36f9b89
MarMeron <meron.hadarr@gmail.com>
2021-04-25 12:10:55 +0300

    push

---
+++ Assets/MousePosScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePosScript : MonoBehaviour
+{
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
+                Camera.main.ScreenToWorldPoint(Input.mousePosition).y,-10) ;
+    }
+}
