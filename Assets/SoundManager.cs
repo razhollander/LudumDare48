9bdd3397a46d6e291386aa9500fe4d6363bd0db9
MarMeron <meron.hadarr@gmail.com>
2021-04-26 17:02:11 +0300

    psh

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource antDie;
+    public AudioSource queenHit;
+    public AudioSource queenDie;
+
+    
+}
